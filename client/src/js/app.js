daa9d76c0716c10daf0741c8a483a1b160917790
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-05-26 14:08:16 -0700

    add sponsor slider on homepage

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,6 +1,6 @@
 const bs = require('bootstrap');
-
 const {WOW} = require('wowjs');
+const Swiper = require('swiper/bundle').default;
 
 [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     .map((el) => new bs.Popover(el));
@@ -14,5 +14,15 @@ const wow = new WOW({
 });
 
 wow.init();
+
+new Swiper('.swiper-container', {
+    loop         : true,
+    slidesPerView: 5,
+    speed        : 1000,
+
+    autoplay: {
+        delay: 8000,
+    }
+});
 
 console.log('Loaded! 😀'); // eslint-disable-line no-console
