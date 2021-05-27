efca656f6cb25e2bf170ee67904de0f8ce673533
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-05-27 14:54:46 -0700

    cleanup and implement a mobile layout

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -22,7 +22,28 @@ new Swiper('.swiper-container', {
 
     autoplay: {
         delay: 8000,
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        },
+        1400: {
+            slidesPerView: 5
+        }
     }
+});
+
+document.getElementById('hamburger').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('open');
+    document.querySelector('#site-header').classList.toggle('open');
 });
 
 console.log('Loaded! 😀'); // eslint-disable-line no-console
