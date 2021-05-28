88d37f19096fc613a4d8c5549a6a5199be10ee69
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-05-28 10:28:54 -0700

    stub out main pages and add breadcrumbs

--- server/locals.js
+++ server/locals.js
@@ -2,4 +2,5 @@ require('dotenv').config();
 
 module.exports = function (app) {
     app.locals.TITLE = process.env.APP_TITLE;
+    app.locals.DISCORD_INVITE = process.env.DISCORD_INVITE;
 };
--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,39 @@ const express = require('express');
 const routes = express.Router(); // eslint-disable-line new-cap
 
 routes.get('/', (req, res) => {
-    res.status(200).render('index');
+    res.render('index');
+});
+
+routes.get('/news', (req, res) => {
+    res.render('news');
+});
+
+routes.get('/streams', (req, res) => {
+    res.render('streams');
+});
+
+routes.get('/sponsorships', (req, res) => {
+    res.render('sponsorships');
+});
+
+routes.get('/division', (req, res) => {
+    res.render('division');
+});
+
+routes.get('/divisions', (req, res) => {
+    res.render('divisions');
+});
+
+routes.get('/application', (req, res) => {
+    res.render('application');
+});
+
+routes.get('/store', (req, res) => {
+    res.render('store');
+});
+
+routes.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 module.exports = routes;
