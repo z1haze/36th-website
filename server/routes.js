e08f8c5a30a16913592eaf27b5c872ebc378e2a0
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-07-19 15:43:43 -0700

    stub out the streams page

--- server/routes.js
+++ server/routes.js
@@ -1,38 +1,47 @@
 const express = require('express');
 const routes = express.Router(); // eslint-disable-line new-cap
 
+// home
 routes.get('/', (req, res) => {
     res.render('index');
 });
 
+// unit history
 routes.get('/history', (req, res) => {
     res.render('history');
 });
 
+// news main
 routes.get('/news', (req, res) => {
     res.render('news');
 });
 
+// news article
 routes.get('/news/:articleSlug', (req, res) => {
     res.render('news/article');
 });
 
+// sponsors
 routes.get('/sponsors', (req, res) => {
     res.render('sponsors');
 });
 
+// streams
 routes.get('/streamers', (req, res) => {
     res.render('streamers');
 });
 
+// application
 routes.get('/apply', (req, res) => {
     res.render('apply');
 });
 
+// merch/store
 routes.get('/shop', (req, res) => {
     res.render('shop');
 });
 
+// contact
 routes.get('/contact', (req, res) => {
     res.render('contact');
 });
