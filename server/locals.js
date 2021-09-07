3ba60ba275a890a97f17aa7095307454b789ca35
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-09-07 16:41:22 -0700

    add discord invitation link

--- server/locals.js
+++ server/locals.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = function (app) {
-    app.locals.TITLE = process.env.APP_TITLE;
+    app.locals.APP_TITLE = process.env.APP_TITLE;
     app.locals.DISCORD_INVITE = process.env.DISCORD_INVITE;
 };
